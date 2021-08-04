571c23125e74facf80afc8b7e2abf2ee29556214
Pedro Rocha <pedro.nec@hotmail.com>
2021-08-04 14:26:01 -0300

    routes tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,8 @@
 module.exports = app => {
     const Tasks = app.models.tasks;
     app.get('/tasks', (req, res) => {
-        res.json({
-            tasks: [
-                {title: 'Fazer compras'},
-                {title: 'Consertar o pc'},
-            ]
-        });
+      Tasks.findAll({}, (tasks) => {
+          res.json({tasks: tasks});
+      });
     });
 };
